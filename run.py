e64ec71e5ce6bac6e7b45625c7d018b7c4ecb75c
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2020-02-15 15:06:39 +0530

    added login function

--- run.py
+++ run.py
@@ -10,5 +10,31 @@ class TinderBot():
     def login(self):
         self.driver.get('https://tinder.com')
 
+        sleep(5)
+
+        fb_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
+        fb_btn.click()
+
+        base_window = self.driver.window_handles[0]
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+
+        email_input = self.driver.find_element_by_xpath('//*[@id="email"]')
+        password_input = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        email_input.send_keys(fb_email)
+        password_input.send_keys(fb_password)
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
+        login_btn.click()
+
+        self.driver.switch_to_window(self.driver.window_handles[0])
+
+        sleep(2)
+        
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_1.click()
+        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_2.click()
+
 bot = TinderBot()
 bot.login()
